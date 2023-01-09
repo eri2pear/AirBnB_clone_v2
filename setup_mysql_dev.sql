f6fe711dcb0d5690586d297f6b18f018d1059aae
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2023-01-10 01:11:29 +0300

    dev fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,10 @@
--- setup test
+-- prepares a MySQL server for the AirBnB clone
+-- create a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- create a user and grant privileges
+GRANT ALL ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant SELECT privileges on performance_schema
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
