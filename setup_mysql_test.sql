21fa2a50987dc9cad5d7839d541541e218fe5523
Patricia Atrisia Rukud'de <eri2pear@gmail.com>
2023-01-10 01:58:55 +0300

    test fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,10 @@
--- setup test
+-- prepares a MySQL test server for the AirBnb clone
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+-- create a user and grant privileges
+GRANT ALL ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+-- grant SELECT privileges
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
